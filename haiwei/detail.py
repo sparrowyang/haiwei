cae22d52e2193267e4b13694a49096a0f57a7934
sparrowyang <sparrooow@qq.com>
2021-05-31 11:38:11 +0800

    添加图片 完善主页按钮 详情界面增加价格 调用随机结构 调用定位接口 调用锅炉接口

--- haiwei/detail.py
+++ haiwei/detail.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def open(request):
     pic_name = request.GET['pic']
     name = request.GET['name']
+    price = request.GET['p']
     #渲染内容
     customlr = request.session.get('customlr',None)
     theme = request.session.get('theme',None)
@@ -11,6 +12,7 @@ def open(request):
         'picture':pic_name,
         'name': name,# 这会将模板中{{title}} 字段渲染为冒号右边的字符串
         'theme':theme,
+        'price':price,
         'customlr': customlr,
         'dec':'啦啦啦啦啦啦了阿三大赛的阿斯顿阿斯顿阿三开机的阿克苏记得很卡死极好的阿卡教授大声疾呼耷拉是耷拉和耷拉是耷拉上来看将散开来极好的'
     }
